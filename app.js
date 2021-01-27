6e21ea0cf9a9233da1611a8222ee6bd3bce4ba46
Matthew VanZoeren <mattvanzoeren@gmail.com>
2021-01-27 11:33:08 -0500

    updated for heroku push

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 
 const homeStartingContent = "Welcome to my site! Here I will try to upload all of my completed projects, as well as keep an updated version of my resume. The goal is to make this site an all-in-one web address to allow for quick and easy access to my programming trials and accomplishments, so that I can give potential employers a concise snapshot of my progress and skills.";
 const aboutContent = "I am a self-taught full stack web developer, after beginning the learning process in June of 2020. Before that, I had worked my way from Field Chemist up to the Operations Manager of Tradebe Environmental's fledgling New Jersey branch in just shy of 2 years. There I learned not only to work with a team, but also to balance problems with expectations, putting my company and their customers above all else. Like so many others, I lost my job around the time of the Covid outbreak, which jumpstarted my path to learning coding. With my Bachelor's in Physics from Rutgers University, I have gained a diverse background of mathematics, science, and problem-solving abilities throughout the years. ";
-const aboutContent2 = "I am currently fluent in HTML, CSS, Javascript, jQuery, Express, node.js, react.js, mySQL, and MongoDB. I am constantly learning new techniques and trying new languages in hopes to broaden my programming horizons. Front and back end web development are both within my scope if needed, and I am always up to the task of learning more. I love working with others, and will always offer my help if I can. Likewise, I am not afraid to ask for help when I need it, as I highly value working together with a team to make something greater than it's constituent parts.";
+const aboutContent2 = "I am currently fluent in HTML, CSS, Javascript, jQuery, Express, PHP, node.js, react.js, mySQL, and MongoDB. I am constantly learning new techniques and trying new languages in hopes to broaden my programming horizons. Front and back end web development are both within my scope if needed, and I am always up to the task of learning more. I love working with others, and will always offer my help if I can. Likewise, I am not afraid to ask for help when I need it, as I highly value working together with a team to make something greater than it's constituent parts.";
 const contactContent = "The best way to contact me is through my email, which is shown below and also included on my resume (resume can be found in the 'About Me' section). My resume includes my cell phone number, also shown below. Thank you for viewing my page, and have a nice day!";
 
 const app = express();
@@ -17,7 +17,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect('mongodb://localhost:27017/blogDB', {useNewUrlParser: true});
+mongoose.connect('mongodb+srv://vmattz:Mollycody1993@cluster0.k41nf.mongodb.net/portfoliodb', {useNewUrlParser: true});
 
 const postSchema = new mongoose.Schema({
   title: String,
@@ -76,11 +76,15 @@ app.get("/posts/:postId", function(req, res){
       content: post.content
     });
   });
-
-
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+
+
+var port = process.env.PORT;
+if (port == null || port == ""){
+  port = 3000;
+}
+app.listen(port, function() {
+  console.log("Server started");
 });
 
